7903190e933b0a5d6ff0f22255e71f6a0f17981e
aman-ka <aman-ka@outlook.com>
2020-04-10 23:12:35 +0530

    Updates

---
+++ precache-manifest.adbc9619cae59f82ad6773abc19e04a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f61ebcfb9cec0f0d3e94113022601f77",
+    "url": "/mosters-rolodex/index.html"
+  },
+  {
+    "revision": "e8900ff7b237a15eb43e",
+    "url": "/mosters-rolodex/static/css/main.503e921f.chunk.css"
+  },
+  {
+    "revision": "0f558170f71217fe2a40",
+    "url": "/mosters-rolodex/static/js/2.7b1bd289.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mosters-rolodex/static/js/2.7b1bd289.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8900ff7b237a15eb43e",
+    "url": "/mosters-rolodex/static/js/main.e97b95fa.chunk.js"
+  },
+  {
+    "revision": "21d13ea94e50a6c69989",
+    "url": "/mosters-rolodex/static/js/runtime-main.7579acb6.js"
+  }
+]);
